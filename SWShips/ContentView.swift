4d4caee9bbdf0a65980962f85ca118379086f5bc
Enric <eherce@estilcolor.com>
2021-09-01 17:35:56 +0200

    Initial Commit

---
+++ SWShips/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SWShips
+//
+//  Created by Enric Herce on 1/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SWShips/SWShipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWShipsApp.swift
+//  SWShips
+//
+//  Created by Enric Herce on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SWShipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
